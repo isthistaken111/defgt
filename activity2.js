a5bf174b1a9cd8b30ff3598daa0d0d8630e24750
isthistaken111 <78668931+isthistaken111@users.noreply.github.com>
2022-02-27 16:31:20 -0500

    Add files via upload

---
+++ activity2.js
@@ -0,0 +1,8 @@
+function back() {
+    window.location="activity_1.html";
+}
+
+function getscore() {
+    score = localStorage.getItem("score");
+    document.getElementById("update").innerHTML="score : " + score;
+}
